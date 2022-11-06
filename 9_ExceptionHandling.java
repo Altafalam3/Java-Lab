e85e84cb68b5a721e7f90dd778eb2f7a8e28e452
Altaf Alam <102013452+Altafalam3@users.noreply.github.com>
2022-11-06 15:02:08 +0530

    Update and rename try2.java to 9_ExceptionHandling.java

--- 9_ExceptionHandling.java
+++ 9_ExceptionHandling.java
@@ -1,11 +1,12 @@
-public class try2{
+public class 9_ExceptionHandling{
 	public static void main(String [] args){
 		try{
 		int a=40/0;
-		System.out.println("rest of the code");
+		System.out.println(a);
 		}
 		catch(Exception e){
 			System.out.println(e);
 		}
+		System.out.println("rest of the code");
 	}
 }
